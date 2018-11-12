2063dd84e1c226421cf8462febe7a26f0b025a45
Martí Bosch <marti.bosch.1992@gmail.com>
2018-11-12 16:11:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+[![PyPI version fury.io](https://badge.fury.io/py/pylandstats.svg)](https://pypi.python.org/pypi/pylandstats/)
+[![Build Status](https://travis-ci.org/martibosch/pylandstats.svg?branch=master)](https://travis-ci.org/martibosch/pylandstats)
+[![Coverage Status](https://coveralls.io/repos/github/martibosch/pylandstats/badge.svg?branch=master)](https://coveralls.io/github/martibosch/pylandstats?branch=master)
+[![GitHub license](https://img.shields.io/github/license/martibosch/pylandstats.svg)](https://github.com/martibosch/pylandstats/blob/master/LICENSE)
+
+pyLandStats
+===============================
+
+Overview
+--------
+
+Open-source Python library to compute landscape metrics
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install pylandstats
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/martibosch/pylandstats.git
+    $ python setup.py install
+
+TODO
+----
+
+* Add the repository to Travis-CI
+* Add the repository to coveralls
+* Register project to PyPI
+* Add encrypted password to `.travis.yml` for automated PyPI releases
---
+++ setup.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+
+from io import open  # compatible enconding parameter
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = '0.0.1'
+
+classifiers = [
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+]
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [
+    x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')
+]
+
+setup(
+    name='pylandstats',
+    version=__version__,
+    description='Open-source Python library to compute landscape metrics',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=classifiers,
+    url='https://github.com/martibosch/pylandstats',
+    author='Martí Bosch',
+    author_email='marti.bosch@epfl.ch',
+    license='GPL-3.0',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+)
