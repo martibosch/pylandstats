30e900ee9443560a88b6555187d482998f6f2897
Martí Bosch <marti.bosch@protonmail.com>
2023-08-24 09:54:03 +0200

    build: update pythran setup for windows

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 """pylandstats setup script."""
 
-import platform
 from pathlib import Path
 
 from setuptools import setup
@@ -11,18 +10,12 @@ from transonic.dist import init_transonic_extensions, make_backend_files
 
 here = Path(__file__).parent.absolute()
 
-if platform.system() == "Windows":
-    backend = "numba"
-else:
-    backend = "pythran"
+backend = "pythran"
 
 paths = ["pylandstats/landscape.py"]
 make_backend_files([here / path for path in paths], backend=backend)
 
-if platform.system() == "Linux":
-    compile_args = ("-O3", "-DUSE_XSIMD")
-else:
-    compile_args = ("-O3",)
+compile_args = ("-O3", "-DUSE_XSIMD")
 
 extensions = init_transonic_extensions(
     "pylandstats", compile_args=compile_args, backend=backend
